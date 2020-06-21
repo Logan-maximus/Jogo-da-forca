b0698522907a0542a20e6a1b79a44dc121fd6315
Nilton Gomes <63253982+Logan-maximus@users.noreply.github.com>
2020-06-21 18:12:41 -0300

    Add files via upload

---
+++ load.py
@@ -0,0 +1,35 @@
+import time, os #Importa o módulo os para uso do cls e time para uso do sleep
+
+def load():
+    '''
+    Está função imprime na tela uma sere de strings apenas para simular um carregamento, se jogado pelo DOS, apresentara
+    uma atualização da tela através do cls
+    '''
+    os.system('cls')
+    print('CARREGANDO [#           ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [##          ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [###         ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [####        ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [#####       ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [######      ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [#######     ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [#########   ]')
+    time.sleep(1)
+    os.system('cls')
+    print('CARREGANDO [############]')
+    time.sleep(1)
+    os.system('cls')
